6ec693f07f729f12eec3a264a280d2ae6b8d18ad
berryjuanda <54657810+berryjuanda@users.noreply.github.com>
2022-03-29 14:29:09 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>
+      Home|Berry Juanda Prawira
+    </title>
+    <body>
+      <h1>
+        <?php
+          echo "Hello Berry";
+        ?>
+      </h1>
+    </body>
+  </head>
+</html>
